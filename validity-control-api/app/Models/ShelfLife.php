6675ab2f9fa8bbdda201e70e243d1a6e5982d852
saucesar <sau.cesarlima2013@gmail.com>
2020-11-14 19:58:01 -0300

    metodo auxuliar de api e referencia para o produto.

--- validity-control-api/app/Models/ShelfLife.php
+++ validity-control-api/app/Models/ShelfLife.php
@@ -11,4 +11,18 @@ class ShelfLife extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['amount', 'date', 'product_id'];
+
+    public function toArray()
+    {
+        return [
+            'amount' => $this->amount,
+            'date' => $this->date,
+            'product_id'=> $this->product_id,
+        ];
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id')->withTrashed();
+    }
 }
