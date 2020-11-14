81a9c7d362ec4f5a9b3cf2992e8c173bed119a63
saucesar <sau.cesarlima2013@gmail.com>
2020-11-14 16:32:01 -0300

    descrição desmarcada como campo unico.

--- validity-control-api/database/migrations/2020_11_14_033215_create_products_table.php
+++ validity-control-api/database/migrations/2020_11_14_033215_create_products_table.php
@@ -11,7 +11,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('barcode')->unique();
-            $table->string('description')->unique();
+            $table->string('description');
             $table->timestamps();
             $table->softDeletes();
         });
