bd0b13fb2b5598ce9cde5f1b90f642b3263d803a
saucesar <sau.cesarlima2013@gmail.com>
2020-11-14 00:58:41 -0300

    controller de product com index funcional.

---
+++ validity-control-api/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
