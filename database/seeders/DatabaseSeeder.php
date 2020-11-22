6eeb0001450f36d8ac41188909db183a7b6ec4ba
saucesar <sau.cesarlima2013@gmail.com>
2020-11-21 22:01:45 -0300

    adição de user padrão.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -10,6 +11,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'cesar',
+            'email' => 'cesar@vc.com',
+            'password' => bcrypt('123456'),
+        ]);
+        
         $this->call(CompanySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(ShelfLifeSeeder::class);
