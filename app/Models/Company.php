3e8dc0ddb9c9d64a5659beee512a52cf76f2c277
saucesar <sau.cesarlima2013@gmail.com>
2020-11-21 21:19:15 -0300

    metodos para busca de produtos por empresa.

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'company_id', 'id');
+    }
+
+    public function toArray()
+    {
+        return [
+            'name' => $this->name,
+            'products' => $this->products,
+        ];
+    }
 }
