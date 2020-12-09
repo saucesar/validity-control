8765de1a07289422d91b811a78edd3aed020d05e
saucesar <sau.cesarlima2013@gmail.com>
2020-12-09 14:10:14 -0300

    barra de navegação padrão.

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <a class="navbar-brand" href="{{ '' }}">
+        <i class="fab fa-ethereum"></i>
+        <small>VC</small>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item nav-lg {{ !isset($active) ? 'active' : '' }}">
+                <a class="nav-link btn-sm btn-primary" href="{{ '' }}">
+                    <i class="fas fa-home"></i>
+                    Home
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
