9c5117141fc7616f9d1a79db08fa74c4714bd87c
saucesar <sau.cesarlima2013@gmail.com>
2020-12-09 14:39:58 -0300

    criada página inicial

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-5 text-center">
+        <br>
+        <h1>Home</h1>
+        @include('components.messages')
+    </div>
+@endsection
