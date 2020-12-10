d4b8ae2cd6a659a213e8346fb443cfbb7b337861
saucesar <sau.cesarlima2013@gmail.com>
2020-12-10 20:21:37 -0300

    remoção de import não utilizado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
         $user = Auth::user();
+        
         $params = [
             'user' => $user,
             'products' => Product::where('company_id', $user->company->id)
