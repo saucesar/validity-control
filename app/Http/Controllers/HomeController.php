5b6eaf7933a13e85d39b1f51a6cf9d93c3fca705
saucesar <sau.cesarlima2013@gmail.com>
2020-12-09 22:41:38 -0300

    metodo index como exibição de dados de produtos.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home/index');
+        $user = Auth::user();
+        $params = [
+            'user' => $user,
+            'products' => Product::where('company_id', $user->company->id)->paginate(10),
+        ];
+
+        return view('home/index', $params);
     }
 }
