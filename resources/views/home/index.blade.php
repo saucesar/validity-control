c54400009a4dae481178228c13035461c041c930
saucesar <sau.cesarlima2013@gmail.com>
2020-12-10 23:58:01 -0300

    adição de quantidade de dias na pesquisa

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,19 +12,26 @@
         <div class="row text-center">
             <div class="col">{{ $user->company->name }}</div>
         </div>
-        <div class="row d-flex justify-content-center mt-5 mb-5">
-            <form class="form-inline my-2 my-lg-0 mr-2" action="{{ route('products.search') }}" method="POST">
-                @csrf
-                <div class="input-group">
-                    <input class="form-control form-control" type="search" name="search"
-                        placeholder="{{__('Faça uma busca')}}" required>
-                    <input type="hidden" name='user_id' value="{{ $user->id }}">
-                    <input type="hidden" name='webmode' value="true">
-                    <div class="input-group-append">
-                        <button class="btn btn-success btn-sm my-2 my-sm-0" type="submit">{{__('Buscar')}}</button>
+        <div class="row mt-5 mb-2">
+            <div class="col d-flex justify-content-center">
+                <form class="form-inline" action="{{ route('products.search') }}" method="POST">
+                    @csrf
+                    <div class="input-group">
+                        <input class="form-control form-control" type="search" name="search" placeholder="{{__('Faça uma busca')}}">
+                        <select name="days" class="form-control">
+                            <option value="3">3 dias</option>
+                            <option value="15">15 dias</option>
+                            <option value="30" selected>30 dias</option>
+                            <option value="45">45 dias</option>
+                            <option value="60">60 dias</option>
+                            <option value="90">90 dias</option>
+                        </select>
+                        <div class="input-group-append">
+                            <button class="btn btn-success btn-sm my-2 my-sm-0" type="submit">{{__('Buscar')}}</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
         <div class="row">
             <div class="col">
@@ -35,46 +42,9 @@
             <div class="col">
                 @if(isset($products))
                     @foreach($products as $product)
-                    <div class="card mb-2">
-                        <div class="card-body row d-flex justify-content-between">
-                            <div class="col">
-                                <h5 class="card-title">{{ $product->description }}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{{ $product->barcode }}</h6>
-                            </div>
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseproduct{{ $product->id }}" aria-expanded="false">
-                                <i class="far fa-caret-square-down"></i>
-                            </button>
-                        </div>
-                        <div class="collapse" id="collapseproduct{{ $product->id }}">
-                            <table class="table table-responsive table-hover table-borderless">
-                                <thead>
-                                    <th>Data</th>
-                                    <th>Quantidade</th>
-                                    <th></th>
-                                </thead>
-                                <tbody>
-                                    @foreach($product->expirationDates as $expdate)
-                                    <tr>
-                                        <td>{{ $expdate['date'] }}</td>
-                                        <td>{{ $expdate['amount'] }}</td>
-                                        <td>
-                                            <form action="{{ route('product.removeDate', $expdate) }}" method="post">
-                                                @csrf
-                                                @method('delete')
-
-                                                <button type="submit" style="zoom: 60%;" class="btn btn-sm btn-outline-danger" onclick="return confirm('Deseja remover?');">
-                                                    <i class="fas fa-trash"></i>
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    @include('components.modalAddDate', ['product' => $product])
-                @endforeach
+                        @include('components.card_product', ['product' => $product])
+                        @include('components.modalAddDate', ['product' => $product])
+                    @endforeach
                 @else
                 <div class="row text-center">
                     <div class="col">
