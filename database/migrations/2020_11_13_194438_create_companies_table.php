b3013eaa2170195924453ac1ca9a01f86b24b808
saucesar <sau.cesarlima2013@gmail.com>
2020-12-11 14:16:56 -0300

    adição de campo owner_id

--- database/migrations/2020_11_13_194438_create_companies_table.php
+++ database/migrations/2020_11_13_194438_create_companies_table.php
@@ -12,6 +12,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('owner_id');
             $table->timestamps();
             $table->softDeletes();
         });
