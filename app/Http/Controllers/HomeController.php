58671e2a423f4a0807244cd94ee9be9e91d8da90
saucesar <sau.cesarlima2013@gmail.com>
2020-12-12 00:05:40 -0300

    metodos transferidos para o model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,28 +13,10 @@ class HomeController extends Controller
 
         $params = [
             'user' => $user,
-            'products' => $this->getProducts($user),
-            'access_requests' => $this->getAccessRequests($user),
+            'products' => $user->getProducts(),
+            'access_requests' => $user->getAccessRequests(),
         ];
 
         return view('home/index', $params);
-    }
-
-    private function getProducts($user)
-    {
-        return $user->access_granted ? Product::where('company_id', $user->company->id)->orderBy('description')->paginate(15) : null;
-    }
-
-    private function getAccessRequests($user)
-    {
-        if(!$user->isCompanyOwner()){
-            return null;
-        }
-        
-        $requests = User::where('company_id', $user->company->id)
-                        ->where('access_granted', false)
-                        ->where('access_denied', false);
-        
-        return $requests->exists() ? $requests->get() : null;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -37,5 +38,38 @@ class User extends Authenticatable
     public function isCompanyOwner()
     {
         return ($this->company->owner->id == $this->id);
+    }
+
+    public function getProducts()
+    {
+        return $this->access_granted ? Product::where('company_id', $this->company->id)->orderBy('description')->paginate(15) : null;
+    }
+
+    public function getProductsByExpiration($days)
+    {
+        $initial_date = Carbon::now();
+        $final_date = Carbon::now()->addDays($days);
+
+        return $this->access_granted ? Product::where('company_id', $this->company->id)
+                                              ->join('expiration_dates', 'expiration_dates.product_id', '=', 'products.id')
+                                              ->whereDate('expiration_dates.date', '>=', $initial_date)
+                                              ->whereDate('expiration_dates.date', '<=', $final_date)
+                                              ->distinct(['products.id'])
+                                              ->select('products.*')
+                                              ->paginate(15)
+                                     : null;
+    }
+
+    public function getAccessRequests()
+    {
+        if(!$this->isCompanyOwner()){
+            return null;
+        }
+
+        $requests = User::where('company_id', $this->company->id)
+                        ->where('access_granted', false)
+                        ->where('access_denied', false);
+        
+        return $requests->exists() ? $requests->get() : null;
     }
 }
