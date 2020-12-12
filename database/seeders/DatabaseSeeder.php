8f31f3a4071149810a74d5c7b07f0c0cc72cd5e4
saucesar <sau.cesarlima2013@gmail.com>
2020-12-11 22:12:28 -0300

    correção na ordem do seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,19 @@ class DatabaseSeeder extends Seeder
             $this->call(CompanySeeder::class);
             $this->call(ProductSeeder::class);
             $this->call(ExpirationDateSeeder::class);
+        }
 
+        DB::table('users')->insert([
+            'name' => 'cesar',
+            'email' => 'cesar@vc.com',
+            'password' => bcrypt('123456'),
+            'company_id' => 1,
+            'access_granted' => true,
+        ]);
+
+        if(env('APP_ENV') == 'local'){
             DB::table('users')->insert([
-                'name' => 'cesar',
+                'name' => 'cesar2',
                 'email' => 'cesar2@vc.com',
                 'password' => bcrypt('123456'),
                 'company_id' => 1,
@@ -41,13 +51,5 @@ class DatabaseSeeder extends Seeder
                 'access_granted' => false,
             ]);
         }
-
-        DB::table('users')->insert([
-            'name' => 'cesar',
-            'email' => 'cesar@vc.com',
-            'password' => bcrypt('123456'),
-            'company_id' => 1,
-            'access_granted' => true,
-        ]);
     }
 }
