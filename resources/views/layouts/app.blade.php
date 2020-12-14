14f5f1f528535c293d8fc97442e6cdf295b7b7b8
saucesar <sau.cesarlima2013@gmail.com>
2020-12-14 14:44:10 -0300

    adição de link para font awasome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('bootstrap-4.5.2') }}/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css') }}/app.css">
-    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+          integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
     @stack('head_scripts')
 
     <title>{{ $title ?? 'VC' }}</title>
