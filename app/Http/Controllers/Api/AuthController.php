0a4a7b0e34922897d1a4e3d0e619e5b24d3162ef
saucesar <sau.cesarlima2013@gmail.com>
2020-12-22 21:49:27 -0300

    controller para autorização de acesso via api.

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        $token = Auth::guard('api')->attempt($credentials);
+
+        if ($token) {
+            return $this->respondWithToken($token);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function me()
+    {
+        return response()->json(guard('api')->user());
+    }
+
+    public function logout()
+    {   
+        Auth::guard('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60,
+        ]);
+    }
+}
