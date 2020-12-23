5fb4e7493ea9893284f9e897492cc8b0e7de4634
saucesar <sau.cesarlima2013@gmail.com>
2020-12-22 23:50:34 -0300

    tratamento de erro para api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -32,6 +33,10 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+        $this->renderable(function (ValidationException $e, $request) {
+            if($request->is('api/*')){
+                return response()->json(['errors' => $e->errors(),], $e->status);
+            }
+        });
     }
 }
