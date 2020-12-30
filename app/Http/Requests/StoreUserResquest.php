de537fdfe37cc8a1526bc80953682633a4b15388
saucesar <sau.cesarlima2013@gmail.com>
2020-12-30 14:19:01 -0300

    criação de requests de validação para criar/atualizar usuários.

---
+++ app/Http/Requests/StoreUserResquest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserResquest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5',
+            'email' => 'required|email:rfc|unique:users',
+            'company' => 'required',
+            'password' => 'required|min:6|confirmed',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateUserResquest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserResquest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5',
+            'oldpass' => 'required',
+            'newpass' => 'required|min:6|confirmed',
+        ];
+    }
+}
