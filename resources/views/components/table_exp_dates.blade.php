a3ba44f9e7ac4d0d8ed6a7c67450d667db6245fe
saucesar <sau.cesarlima2013@gmail.com>
2020-12-30 00:21:59 -0300

    correção para não exibir dias a vencer em historico.

--- resources/views/components/table_exp_dates.blade.php
+++ resources/views/components/table_exp_dates.blade.php
@@ -1,13 +1,15 @@
-<table class="table table-hover table-borderless text-center {{ $table_historic_class ?? '' }}">
+<table class="table table-hover table-borderless text-center zoom-90 {{ $table_historic_class ?? '' }}">
     <thead>
         <th>
             <i class="far fa-calendar-alt"></i>
             Data
         </th>
+        @if(!isset($is_historic))
         <th>
             <i class="far fa-clock"></i>
             Dias Restantes
         </th>
+        @endif
         <th>
             <i class="fas fa-boxes"></i>
             Quantidade
@@ -29,23 +31,30 @@
         @foreach($dates as $expdate)
         <tr>
             <td>{{ $expdate->date }}</td>
+            @if(!isset($is_historic))
             <td>{{ $expdate->daysToExpire() }}</td>
+            @endif
             <td>{{ $expdate->amount }}</td>
             <td>{{ $expdate->lote }}</td>
             <td>
-                @if(!isset($is_historic))
-                <form action="{{ route('product.removeDate', $expdate) }}" method="post">
-                    @csrf
-                    @method('delete')
-
-                    <button type="submit" style="zoom: 60%;" class="btn btn-sm btn-outline-danger"
-                        onclick="return confirm('Deseja remover?');" title="remover {{ $expdate->date }} ?">
-                        <i class="fas fa-trash"></i>
+            @if(!isset($is_historic))
+                <div class="btn-group" role="group">
+                    <button class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#modalEditDate{{ $expdate->id }}">
+                        <i class="far fa-edit"></i>
                     </button>
-                </form>
-                @endif
+                    <form action="{{ route('product.removeDate', $expdate) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-sm btn-outline-danger"onclick="return confirm('Deseja remover?');"
+                                title="remover {{ $expdate->date }} ?">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </form>
+                </div>
+            @endif
             </td>
         </tr>
+        @include('components.modalEditDate', ['expdate' => $expdate])
         @endforeach
     </tbody>
 </table>
