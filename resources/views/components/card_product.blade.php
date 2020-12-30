fbb654f6f1933f94cc5db5b4ac02657961d8e77d
saucesar <sau.cesarlima2013@gmail.com>
2020-12-29 21:48:23 -0300

    exibindo marcador colorido a depender da quantidade de dias do produto.

--- resources/views/components/card_product.blade.php
+++ resources/views/components/card_product.blade.php
@@ -1,10 +1,11 @@
 @include('components.modalAddDate', ['product' => $product])
 
-<div class="card mb-2">
+<div class="card mb-2" >
     <div class="card-body row d-flex justify-content-between">
         <div class="col">
             <h5 class="card-title" title="Detalhes do produto"><a href="{{ route('products.show', $product) }}">{{ $product->description }}</a></h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ $product->barcode }}</h6>
+            <span class="btn  {{ $product->expirationDates->first() ? $product->expirationDates->first()->statusClass() : '' }}"></span>
         </div>
         @if(!isset($collapse_class ))
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseproduct{{ $product->id }}" aria-expanded="false"
