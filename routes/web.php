de9dd2f414bb5b594ccdc84f55da77b8ed135fad
saucesar <sau.cesarlima2013@gmail.com>
2021-01-05 19:44:13 -0300

    chamadas de methodo padronizadas de acordo com o framework.

--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,33 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', HomeController::class.'@checkLogin')->name('login');
+Route::get('/', [HomeController::class, 'checkLogin'])->name('login');
 
-Route::post('users/login', UserController::class.'@login')->name('users.login');
+Route::post('users/login', [UserController::class, 'login'])->name('users.login');
 Route::resource('users', UserController::class)->except(['edit', 'index', 'show']);
 
 Auth::routes(['verify' => true]);
 
 Route::middleware(['auth', 'verified'])->group(function (){
-    Route::get('/home', HomeController::class."@index")->name('home.index');
+    Route::get('/home', [HomeController::class, "index"])->name('home.index');
 
     Route::prefix('users')->group(function(){
-        Route::post('change-password/{user}', UserController::class.'@changePassword')->name('users.changePassword');
-        Route::post('logout', UserController::class.'@logout')->name('users.logout');
-        Route::get('access-request/{user}/{status}', UserController::class.'@accessRequest')->name('users.accessRequest');
-        Route::get('info', UserController::class.'@information')->name('users.information');
+        Route::post('change-password/{user}', [UserController::class, 'changePassword'])->name('users.changePassword');
+        Route::post('logout', [UserController::class, 'logout'])->name('users.logout');
+        Route::get('access-request/{user}/{status}', [UserController::class, 'accessRequest'])->name('users.accessRequest');
+        Route::get('info', [UserController::class, 'information'])->name('users.information');
     });
     
     Route::prefix('company')->group(function(){
-        Route::post('update/{company}', CompanyController::class.'@update')->name('company.update')->middleware(['authorization']);
+        Route::post('update/{company}', [CompanyController::class, 'update'])->name('company.update')->middleware(['authorization']);
     });
 
     Route::resource('products', ProductController::class)->except(['edit'])->middleware(['authorization']);
     Route::prefix('products')->group(function(){
-        Route::post('add-date/{product}', ProductController::class.'@addDate')->name('product.addDate');
-        Route::put('edit-date/{expdate}', ProductController::class.'@updateExpirationDate')->name('product.updateExpdate');
-        Route::match(['get', 'post'], 'search', ProductController::class.'@generalSearch')->name('products.search')->middleware(['user.granted']);
-        Route::delete('remove-date/{expiration_date}', ProductController::class.'@removeDate')->name('product.removeDate');
-        Route::match(['get', 'post'], 'by-expiration-days/{days?}',  ProductController::class.'@expirationDays')->name('products.byExpiration');    
+        Route::post('add-date/{product}', [ProductController::class, 'addDate'])->name('product.addDate');
+        Route::put('edit-date/{expdate}', [ProductController::class, 'updateExpirationDate'])->name('product.updateExpdate');
+        Route::match(['get', 'post'], 'search', [ProductController::class, 'generalSearch'])->name('products.search')->middleware(['user.granted']);
+        Route::delete('remove-date/{expiration_date}', [ProductController::class, 'removeDate'])->name('product.removeDate');
+        Route::match(['get', 'post'], 'by-expiration-days/{days?}',  [ProductController::class, 'expirationDays'])->name('products.byExpiration');    
     });
 });
