66afe018d908ec0733f410cc58be4e780a765a8d
saucesar <sau.cesarlima2013@gmail.com>
2021-01-09 19:44:48 -0300

    componentização do hsitorico de datas.

---
+++ resources/views/components/exp_dates/card_date_historic.blade.php
@@ -0,0 +1,19 @@
+<div class="card card-body min-card-width mb-4">
+    <div class="row">
+        <div class="col">
+            <h5 class="card-title">
+                <i class="far fa-file-alt"></i>
+                Historico de datas
+            </h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col min-card-height-2x">
+            @if(count($historic) > 0)
+            @include('components.exp_dates.table_exp_dates', ['dates' => $dates, 'is_historic' => true])
+            @else
+            <p>Nada por aqui...</p>
+            @endif
+        </div>
+    </div>
+</div>
