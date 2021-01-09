4623f8823d1514eebaa5b2a97add93222c2df505
saucesar <sau.cesarlima2013@gmail.com>
2021-01-09 19:46:03 -0300

    exibindo contagem das datas

--- resources/views/components/exp_dates/card_exp_dates.blade.php
+++ resources/views/components/exp_dates/card_exp_dates.blade.php
@@ -1,4 +1,4 @@
-<div class="card card-body min-card-width">
+<div class="card card-body min-card-width mb-5 shadow">
     <div class="row">
         <div class="col">
             <small><b>{{ $title ?? '' }}</b></small>
@@ -6,13 +6,15 @@
     </div>
     <table class="table critical-date-table">
         <thead>
+            <th>#</th>
             <th>Data</th>
             <th>Quantidade</th>
             <th>Produto</th>
         </thead>
         <tbody>
-            @foreach($dates as $date)
+            @foreach($dates as $key => $date)
             <tr>
+                <td>{{ ++$key }}</td>
                 <td>{{ $date->date }}</td>
                 <td>{{ $date->amount }}</td>
                 <td><a href="{{ route('products.show', $date->product->id) }}">{{ $date->product->description }}</a>
