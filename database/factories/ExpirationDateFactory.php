2fb4d63560a9318fb6c4c27bcc07ed8f3ff6a411
saucesar <sau.cesarlima2013@gmail.com>
2021-01-09 19:45:33 -0300

    ajuste no seeder.

--- database/factories/ExpirationDateFactory.php
+++ database/factories/ExpirationDateFactory.php
@@ -15,7 +15,7 @@ class ExpirationDateFactory extends Factory
     public function definition()
     {
         return [
-            'date' => Carbon::now()->addDays($this->faker->numberBetween(10, 60)),
+            'date' => ($this->faker->numberBetween(0, 3)%2 == 0 ) ? Carbon::now()->addDays($this->faker->numberBetween(10, 60)) : Carbon::now()->subDays($this->faker->numberBetween(1, 3)),
             'amount' => $this->faker->numberBetween(100, 999),
             'lote' => Str::random(10),
             'product_id' => $this->faker->numberBetween(1, 99),
