c76b6296c56a9dc04f7eb204cd2c8da4e6776dbf
saucesar <sau.cesarlima2013@gmail.com>
2021-01-09 21:27:56 -0300

    ajuste de posição do elementos.

--- resources/views/components/products/card_product.blade.php
+++ resources/views/components/products/card_product.blade.php
@@ -2,17 +2,19 @@
 
 <div class="card shadow mb-4" >
     <div class="card-body row d-flex justify-content-between">
-        <div class="col">
+        <div class="col-9">
             <h5 class="card-title" title="Detalhes do produto"><a href="{{ route('products.show', $product) }}">{{ $product->description }}</a></h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ $product->barcode }}</h6>
-            <span class="btn  {{ $product->expirationDates->first() ? $product->expirationDates->first()->statusClass() : '' }}"></span>
+            <span class="btn {{ $product->expirationDates->first() ? $product->expirationDates->first()->statusClass() : '' }}"></span>
         </div>
-        @if(!isset($collapse_class ))
+        <div class="col-1">
+        @if(!isset($collapse_class))
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseproduct{{ $product->id }}" aria-expanded="false"
                 title="Exibir datas.">
             <i class="far fa-caret-square-down"></i>
         </button>
         @endif
+        </div>
     </div>
     <div class="collapse multi-collapse {{ $collapse_class ?? '' }}" id="collapseproduct{{ $product->id }}">
         <div class="row">
@@ -26,4 +28,8 @@
             </div>
         </div>
     </div>
+    <!--
+    <div class="card-footer">
+        <span class="btn btn-block {{ $product->expirationDates->first() ? $product->expirationDates->first()->statusClass() : '' }}"></span>
+    </div>-->
 </div>
