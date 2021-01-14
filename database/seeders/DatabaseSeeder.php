879ed3403cad2cb1dcd1b6e9084ae86568747d4a
saucesar <sau.cesarlima2013@gmail.com>
2021-01-14 13:39:15 -0300

    ajuste no seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,18 +25,23 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        if(env('APP_ENV') == 'local'){
-            for($i = 1; $i <= 10; $i++){
-                DB::table('users')->insert([
-                    'name' => "cesar$i",
-                    'email' => "cesar$i@vc.com",
-                    'password' => bcrypt('123456'),
-                    'company_id' => 1,
-                    'access_granted' => false,
-                    'email_verified_at' => now(),
-                ]);
-            }
+        for($i = 1; $i <= 10; $i++){
+            DB::table('users')->insert([
+                'name' => "cesar$i",
+                'email' => "cesar$i@vc.com",
+                'password' => bcrypt('123456'),
+                'company_id' => 1,
+                'access_granted' => false,
+                'email_verified_at' => now(),
+            ]);
+
+            DB::table('products')->insert([
+                'barcode' => "78911112222$i",
+                'description' => Str::random(),
+                'company_id' => 1,
+            ]);
         }
+
         
         if(env('APP_ENV') == 'local'){
             $this->call(CompanySeeder::class);
