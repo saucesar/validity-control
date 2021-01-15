e57fc34d3f5561acb54c2cb779ba31b02b558a9a
saucesar <sau.cesarlima2013@gmail.com>
2021-01-15 01:10:22 -0300

    criado model para categoria.

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'send_to', 'company_id'];
+
+    protected $casts = ['send_to' => 'array'];
+}
