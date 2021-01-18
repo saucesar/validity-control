54052daab681d3794c5d009e247d9de82311fc80
saucesar <sau.cesarlima2013@gmail.com>
2021-01-18 20:46:43 -0300

    card de legenda de cores separedo em arquivo.

---
+++ resources/views/components/card_color_legend.blade.php
@@ -0,0 +1,23 @@
+<div class="card shadow mb-4">
+    <div class="card-header">
+        <b>Legenda</b>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-2"><span class="btn bg-red"></span></div>
+            <div class="col-10"><small>menos de 10 dias</small></div>
+        </div>
+        <div class="row">
+            <div class="col-2"><span class="btn bg-orange"></span></div>
+            <div class="col-10"><small>menos de 20 dias</small></div>
+        </div>
+        <div class="row">
+            <div class="col-2"><span class="btn bg-light-green"></span></div>
+            <div class="col-10"><small>menos de 30 dias</small></div>
+        </div>
+        <div class="row">
+            <div class="col-2"><span class="btn bg-green"></span></div>
+            <div class="col-10"><small>mais que 30 dias</small></div>
+        </div>
+    </div>
+</div>
