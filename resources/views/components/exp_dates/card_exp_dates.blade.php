6fe2092331af03b88cbb7ba6c218add63452f9c0
saucesar <sau.cesarlima2013@gmail.com>
2021-01-18 14:21:48 -0300

    destaque de titulo a depender da validade.

--- resources/views/components/exp_dates/card_exp_dates.blade.php
+++ resources/views/components/exp_dates/card_exp_dates.blade.php
@@ -1,26 +1,34 @@
-<div class="card card-body min-card-width mb-5 shadow">
-    <div class="row">
-        <div class="col">
-            <small><b>{{ $title ?? '' }}</b></small>
-        </div>
+<div class="card min-card-width mb-5 shadow">
+    <div class="card-header {{ isset($danger) ? 'text-danger' : 'text-warning' }}">
+        @if(isset($danger))
+        <i class="fas fa-exclamation-triangle"></i>
+        @else
+        <i class="fas fa-exclamation-circle"></i>
+        @endif
+        <b>{{ $title ?? '' }}</b>
     </div>
-    <table class="table critical-date-table">
-        <thead>
-            <th>#</th>
-            <th>Data</th>
-            <th>Quantidade</th>
-            <th>Produto</th>
-        </thead>
-        <tbody>
-            @foreach($dates as $key => $date)
-            <tr>
-                <td>{{ ++$key }}</td>
-                <td>{{ $date->date() }}</td>
-                <td>{{ $date->amount }}</td>
-                <td><a href="{{ route('products.show', $date->product->id) }}">{{ $date->product->description }}</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="card-body">
+        <table class="table critical-date-table">
+            <thead>
+                <th>#</th>
+                <th>Data</th>
+                <th>Quantidade</th>
+                <th>Produto</th>
+            </thead>
+            <tbody>
+                @foreach($dates as $key => $date)
+                <tr>
+                    <td>{{ ++$key }}</td>
+                    <td>{{ $date->date() }}</td>
+                    <td>{{ $date->amount }}</td>
+                    <td><a href="{{ route('products.show', $date->product->id) }}">{{ $date->product->description }}</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="card-footer">
+        Total de itens: {{ count($dates) }}
+    </div>
 </div>
