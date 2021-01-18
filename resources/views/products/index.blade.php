14e69693daa4c4557e27410ccd43ba828a7393e0
saucesar <sau.cesarlima2013@gmail.com>
2021-01-18 20:47:42 -0300

    uso de card de legenda componentizado.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -51,26 +51,7 @@
                     </div>
                 </div>
                 @endif
-                <div class="card shadow card-body mb-4">
-                    <div class="col">
-                        <div class="row">
-                            <div class="col-2"><span class="btn bg-red"></span></div>
-                            <div class="col-10"><small>menos de 10 dias</small></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-2"><span class="btn bg-orange"></span></div>
-                            <div class="col-10"><small>menos de 20 dias</small></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-2"><span class="btn bg-light-green"></span></div>
-                            <div class="col-10"><small>menos de 30 dias</small></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-2"><span class="btn bg-green"></span></div>
-                            <div class="col-10"><small>mais que 30 dias</small></div>
-                        </div>
-                    </div>
-                </div>
+                @include('components.card_color_legend')
             </div>
             <div class="col-6">
                 @include('components.products.products_list')
@@ -108,47 +89,43 @@
                         </div>
                     </div>
                 </div>
-                <div class="card shadow card-body min-card-width mb-4">
-                    <div class="row">
-                        <div class="col">
-                            <small><b>Produtos para os próximos ...</b></small>
-                        </div>
+                <div class="card shadow min-card-width mb-4">
+                    <div class="card-header">
+                        <b>Produtos para os próximos ...</b>
                     </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="btn-group" role="group">
-                                <form class="mr-1" action="{{ route('products.byExpiration', 10) }}" method="post">
+                    <div class="card-body ">
+                        <div class="btn-group" role="group">
+                            <form class="mr-1" action="{{ route('products.byExpiration', 10) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-secondary">10 dias</button>
+                             </form>
+                            <form class="mr-1" action="{{ route('products.byExpiration', 15) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-secondary">15 dias</button>
+                            </form>
+                            <form class="mr-1" action="{{ route('products.byExpiration', 30) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-secondary">30 dias</button>
+                            </form>
+                            <form class="mr-1" action="{{ route('products.byExpiration', 45) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-secondary">45 dias</button>
+                            </form>
+                            <form class="mr-1" action="{{ route('products.byExpiration', 60) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-secondary">60 dias</button>
+                            </form>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col">
+                                <form action="{{ route('products.byExpiration') }}" method="post">
                                     @csrf
-                                    <button class="btn btn-sm btn-outline-secondary">10 dias</button>
-                                </form>
-                                <form class="mr-1" action="{{ route('products.byExpiration', 15) }}" method="post">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-secondary">15 dias</button>
-                                </form>
-                                <form class="mr-1" action="{{ route('products.byExpiration', 30) }}" method="post">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-secondary">30 dias</button>
-                                </form>
-                                <form class="mr-1" action="{{ route('products.byExpiration', 45) }}" method="post">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-secondary">45 dias</button>
-                                </form>
-                                <form class="mr-1" action="{{ route('products.byExpiration', 60) }}" method="post">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-secondary">60 dias</button>
+                                    <div class="btn-group" role="group">
+                                        <input type="number" name="days" class="form-control" placeholder="Insira os dias">
+                                        <button class="btn btn-success">Ok</button>
+                                    </div>
                                 </form>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col">
-                            <form action="{{ route('products.byExpiration') }}" method="post">
-                                @csrf
-                                <div class="btn-group" role="group">
-                                    <input type="number" name="days" class="form-control" placeholder="Insira os dias">
-                                    <button class="btn btn-success">Ok</button>
-                                </div>
-                            </form>
                         </div>
                     </div>
                 </div>
