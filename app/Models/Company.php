5a87f20abeb36aa448240ffa9702c96644fe0be5
saucesar <sau.cesarlima2013@gmail.com>
2021-01-18 23:20:49 -0300

    adicao de metodo de relacionamento com categorias.

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -17,6 +17,11 @@ class Company extends Model
         return $this->hasMany(Product::class, 'company_id', 'id');
     }
 
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'company_id', 'id');
+    }
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'owner_id', 'id');
