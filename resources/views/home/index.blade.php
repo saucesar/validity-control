7c657074b9135990881f840a4b4039c9b8cb03e3
saucesar <sau.cesarlima2013@gmail.com>
2021-01-18 21:18:43 -0300

    utilização de componentes de card para solicitações de acesso e usuários permitidos.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -26,78 +26,20 @@
         <div class="row mt-4 d-flex justify-content-start">
             <div class="col-4">
                 @if(isset($critical_dates) && count($critical_dates) > 0)
-                @include('components.exp_dates.card_exp_dates', ['dates' => $critical_dates, 'title' => 'Produtos em data critica ( 3 dias )'])
+                    @include('components.exp_dates.card_exp_dates', ['dates' => $critical_dates, 'title' => 'Produtos em data critica ( 3 dias )'])
                 @endif
                 @if(isset($expired_products) && count($expired_products) > 0)
-                @include('components.exp_dates.card_exp_dates', ['dates' => $expired_products, 'title' => 'Produtos vencidos', 'danger' => true])
+                    @include('components.exp_dates.card_exp_dates', ['dates' => $expired_products, 'title' => 'Produtos vencidos', 'danger' => true])
                 @endif
             </div>
             <div class="col-4">
                 @if(isset($users_granted))
-                <div class="card shadow min-card-width">
-                    <div class="card-header">
-                        <i class="fas fa-user"></i>
-                        <b>Usuários permitidos</b>
-                    </div>
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <th><small>Nome</small></th>
-                                <th><small>Email</small></th>
-                                <th><small>Ações</small></th>
-                            </thead>
-                            <tbody>
-                            @foreach($users_granted as $granted)
-                            <tr>
-                                <td><small>{{ $granted->name }}</small></td>
-                                <td><small>{{ $granted->email }}</small></td>
-                                <td>
-                                    <a class="btn btn-sm btn-danger" href="{{ route('users.accessRequest', [$granted, 'denied']) }}" style="zoom: 75%;" title="Revogar acesso.">
-                                        <i class="fas fa-minus-circle"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="card-footer">
-                        Total: {{ count($users_granted) }}
-                    </div>
-                </div>
+                    @include('components.users.card_users_granted', ['users' => $users_granted])
                 @endif
             </div>
             <div class="col-4">
                 @if(isset($access_requests))
-                <div class="card shadow min-card-width">
-                    <div class="card-header">
-                        <i class="fas fa-user-plus"></i>
-                        <b>Solicitações de Acesso</b>
-                    </div>
-                    <div class="card-body ">
-                        <table class="table">
-                            <thead>
-                                <th><small>Nome</small></th>
-                                <th><small>Ações</small></th>
-                            </thead>
-                            <tbody>
-                            @foreach($access_requests as $request)
-                                <tr>
-                                    <td><small>{{ $request->name }}</small></td>
-                                    <td>
-                                        <a class="btn btn-sm btn-success" href="{{ route('users.accessRequest', [$request, 'granted']) }}" style="zoom: 75%;" title="Aprovar acesso.">
-                                            <i class="far fa-thumbs-up"></i>
-                                        </a>
-                                        <a class="btn btn-sm btn-danger" href="{{ route('users.accessRequest', [$request, 'denied']) }}" style="zoom: 75%;" title="Negar acesso.">
-                                            <i class="far fa-thumbs-down"></i>
-                                        </a>
-                                    </td>
-                                </tr>        
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                    @include('components.users.card_access_requests', ['requests' => $access_requests])
                 @endif
             </div>
         </div>
