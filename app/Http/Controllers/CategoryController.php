08977d7e91c07d2dbf50e4c494ff436ea3f46f6b
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-01-30 00:35:09 -0300

    Passagem de categorias do usuário autenticado.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
     public function index()
     {
         $params = [
-            'categories' => Category::all(),
+            'categories' => Auth::user()->company->categoriesPaginated(),
         ];
+
         return view('categories/index', $params);
     }
 
