81aa2e1e83f0019ac1e376168a2db853c8bba23f
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-04 22:19:08 -0300

    Adicação de icones aos inputs.

--- resources/views/components/inputs/input_password.blade.php
+++ resources/views/components/inputs/input_password.blade.php
@@ -1,1 +1,1 @@
-@include('components.inputs.input_text', ['name' => $name, 'label' => $label, 'type' => 'password', 'value' => ($value ?? null)])
+@include('components.inputs.input_text', ['name' => $name, 'label' => $label, 'type' => 'password', 'value' => ($value ?? null), 'prepemd' => $prepend ?? null])
--- resources/views/components/inputs/input_text.blade.php
+++ resources/views/components/inputs/input_text.blade.php
@@ -1,5 +1,14 @@
 <label for="password">{{ $label }}</label>
+@if(isset($prepend))
+<div class="input-group mb-2">
+    <div class="input-group-prepend">
+        <div class="input-group-text"><i class="{{ $prepend }}"></i></div>
+    </div>
+    <input type="{{ $type ?? 'text' }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}" required>
+</div>
+@else
 <input type="{{ $type ?? 'text' }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}" required>
+@endif
 @error($name)
 <small class="text-danger">{{ $message }}</small>
 @enderror
