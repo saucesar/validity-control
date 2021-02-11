3fa3597fb043c414d4ba857d5da33d3e50200f57
saucesar <sau.cesarlima2013@gmail.com>
2021-02-10 21:30:48 -0300

    ajuste na exibição dos emails

--- resources/views/components/categories/card_category.blade.php
+++ resources/views/components/categories/card_category.blade.php
@@ -6,41 +6,48 @@
         </h5>
     </div>
     <div class="card-body">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-                <p>
-                    <a  class="btn btn-sm btn-outline-primary"  class="btn btn-sm btn-outline-primary" href="{{ route('products.byCategory', $category->id) }}">
-                        <i class="fas fa-cubes"></i>
-                        Total de Produtos: {{ count($category->products) }}
-                    </a>
-                </p>
-            </li>
-            <li class="list-group-item">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <p title="Produtos desta categoria serão enviados para este(s) email(s).">
-                            <i class="far fa-envelope"></i>
-                            Enviar para:
-                        </p>    
-                    </div>
-                    <div>
-                        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modalAddEmail{{ $category->id }}"
-                                title="Adicionar email.">
-                            <i class="fas fa-plus-circle"></i>
-                        </button>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col">
+                <a  class="btn btn-sm btn-outline-primary"  class="btn btn-outline-primary" href="{{ route('products.byCategory', $category->id) }}">
+                    <i class="fas fa-cubes"></i>
+                    Total de Produtos: {{ count($category->products) }}
+                </a>
+            </div>
+            <div class="col">
+                <button class="btn btn-link btn-block" type="button" data-toggle="collapse" data-target="#emails{{ $category->id }}">
+                    Emails
+                    <i class="fas fa-caret-down"></i>
+                </button>
+            </div>
+        </div>
+                
+        <div class="collapse" id="emails{{ $category->id }}">
+            <div class="">
                 <div class="row">
                     <div class="col">
-                        <ul>
-                            @foreach($category->emails as $emailcategory)
-                            <li>{{ $emailcategory->email }}</li>
-                            @endforeach
-                        </ul>
+                    <table class="table">
+                        <thead>
+                            <th>Enviar para</th>
+                            <th>
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
+                                    data-target="#modalAddEmail{{ $category->id }}" title="Adicionar email.">
+                                    <i class="fas fa-plus-circle"></i>
+                                </button>
+                            </th>
+                            </thead>
+                            <tbody>
+                                @foreach($category->emails as $emailcategory)
+                                <tr>
+                                    <td>{{ $emailcategory->email }}</td>
+                                    <td></td>
+                                </tr>
+                                @endforeach    
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
     <div class="card-footer">
         Ultima atualização: {{ $category->updated_at }}
