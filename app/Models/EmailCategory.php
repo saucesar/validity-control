17e27fa601b6bccce0f74ebf4dc8a5991124f5ad
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-11 23:24:54 -0300

    correção para incluir categoria deletada.

--- app/Models/EmailCategory.php
+++ app/Models/EmailCategory.php
@@ -14,6 +14,6 @@ class EmailCategory extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class, 'category_id', 'id');
+        return $this->belongsTo(Category::class, 'category_id', 'id')->withTrashed();
     }
 }
