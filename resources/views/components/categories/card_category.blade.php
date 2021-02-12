27f61e1811a789c4279a79994533c7b3c5a09fec
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-11 23:25:44 -0300

    botoes para editar/apagar categoria.

--- resources/views/components/categories/card_category.blade.php
+++ resources/views/components/categories/card_category.blade.php
@@ -1,9 +1,22 @@
 <div class="card min-card-width-2x mb-4">
-    <div class="card-header">
+    <div class="card-header d-flex justify-content-between">
         <h5 class="card-title">
             <i class="fas fa-cube"></i>
             {{ $category->name }}
         </h5>
+        @if(auth()->user()->isCompanyOwner())
+        <div class="btn-group">
+            <button class="btn btn-primary" type="button" title="Editar categoria" data-toggle="modal"
+                    data-target="#modalCategory{{ $category->id }}">
+                <i class="fas fa-pen-square"></i>
+            </button>
+            <form action="{{ route('categories.destroy', $category->id) }}" method="post">
+                @csrf
+                @method('delete')
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza?');">X</button>
+            </form>
+        </div>
+        @endif
     </div>
     <div class="card-body">
         <div class="row mb-3">
@@ -28,7 +41,7 @@
                 </div>
                 <div class="col text-center">
                     <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
-                            data-target="#modalAddEmail{{ $category->id }}" title="Adicionar email.">
+                            data-target="#modalEmail{{ $category->id }}" title="Adicionar email.">
                         <i class="fas fa-plus-circle"></i>
                     </button>
                 </div>
@@ -38,21 +51,29 @@
             <div class="row">
                 <div class="col">{{ $emailcategory->email }}</div>
                 <div class="col text-center">
-                    <form action="{{ route('categories.deleteEmail', $emailcategory->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Tem Certeza?');">
-                            <i class="fas fa-trash"></i>
+                    <div class="btn-group" role="group">
+                        <button class="btn btn-sm btn-warning" type="button" title="Editar produto" data-toggle="modal"
+                                data-target="#modalEditEmail{{ $emailcategory->id }}">
+                            <i class="fas fa-pen-square"></i>
                         </button>
-                    </form>
+                        <form action="{{ route('categories.deleteEmail', $emailcategory->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Tem Certeza?');">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
             <div class="dropdown-divider"></div>
+            @include('components.categories.modal_edit_email', ['emailcategory' => $emailcategory])
             @endforeach
         </div>
     </div>
     <div class="card-footer">
         Ultima atualização: {{ $category->updated_at->format('d-m-Y H:i:s') }}
     </div>
+    @include('components.categories.modal_add', ['category' => $category])
     @include('components.categories.modal_add_email', ['category' => $category])
 </div>
