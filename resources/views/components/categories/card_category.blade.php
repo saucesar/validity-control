86551daa7f29a9c419d75f252a75f9b54bc89721
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-11 21:00:45 -0300

    ajuste da tabela de email.

--- resources/views/components/categories/card_category.blade.php
+++ resources/views/components/categories/card_category.blade.php
@@ -6,59 +6,53 @@
         </h5>
     </div>
     <div class="card-body">
-        <div class="row">
+        <div class="row mb-3">
             <div class="col">
                 <a  class="btn btn-sm btn-link" href="{{ route('products.byCategory', $category->id) }}">
                     <i class="fas fa-cubes"></i>
                     Total de Produtos: {{ count($category->products) }}
                 </a>
             </div>
-            <div class="col">
-                <button class="btn btn-link btn-block" type="button" data-toggle="collapse" data-target="#emails{{ $category->id }}">
-                    Emails
+            <div class="col text-center">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#emails{{ $category->id }}">
+                    Emails({{ count($category->emails) }})
                     <i class="fas fa-caret-down"></i>
                 </button>
             </div>
         </div>
                 
-        <div class="collapse" id="emails{{ $category->id }}">
-            <div class="">
-                <div class="row">
-                    <div class="col">
-                    <table class="table">
-                        <thead>
-                            <th>Enviar para</th>
-                            <th>
-                                <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
-                                    data-target="#modalAddEmail{{ $category->id }}" title="Adicionar email.">
-                                    <i class="fas fa-plus-circle"></i>
-                                </button>
-                            </th>
-                            </thead>
-                            <tbody>
-                                @foreach($category->emails as $emailcategory)
-                                <tr>
-                                    <td>{{ $emailcategory->email }}</td>
-                                    <td>
-                                        <form action="{{ route('categories.deleteEmail', $emailcategory->id) }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Tem Certeza?');">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach    
-                            </tbody>
-                        </table>
-                    </div>
+        <div class="collapse multicollapse" id="emails{{ $category->id }}">
+            <div class="row">
+                <div class="col">
+                    Enviar para
+                </div>
+                <div class="col text-center">
+                    <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
+                            data-target="#modalAddEmail{{ $category->id }}" title="Adicionar email.">
+                        <i class="fas fa-plus-circle"></i>
+                    </button>
                 </div>
             </div>
+            <div class="dropdown-divider"></div>
+            @foreach($category->emails as $emailcategory)
+            <div class="row">
+                <div class="col">{{ $emailcategory->email }}</div>
+                <div class="col text-center">
+                    <form action="{{ route('categories.deleteEmail', $emailcategory->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Tem Certeza?');">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="dropdown-divider"></div>
+            @endforeach
         </div>
     </div>
     <div class="card-footer">
-        Ultima atualização: {{ $category->updated_at }}
+        Ultima atualização: {{ $category->updated_at->format('d-m-Y H:i:s') }}
     </div>
     @include('components.categories.modal_add_email', ['category' => $category])
 </div>
