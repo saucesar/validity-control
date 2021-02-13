9fa33f86aba86dd82d357fc95832d419995a2dd8
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-13 01:24:47 -0300

    ajuste para opção de apenas leitura,

--- resources/views/components/inputs/input_text.blade.php
+++ resources/views/components/inputs/input_text.blade.php
@@ -4,10 +4,12 @@
     <div class="input-group-prepend">
         <div class="input-group-text"><i class="{{ $prepend }}"></i></div>
     </div>
-    <input type="{{ $type ?? 'text' }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}" required>
+    <input type="{{ $type ?? 'text' }}" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}"
+            {{ isset($readonly) ? 'readonly' : '' }} required>
 </div>
 @else
-<input type="{{ $type ?? 'text' }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}" required>
+<input type="{{ $type ?? 'text' }}" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ $value ?? old($name) }}"
+        {{ isset($readonly) ? 'readonly' : '' }} required>
 @endif
 @error($name)
 <small class="text-danger">{{ $message }}</small>
