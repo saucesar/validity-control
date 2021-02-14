342a9a7ee6ca12ecb4d273ca8bd4adb7410cf66f
igorcalbuquerque <Igor.cezar@ufrpe.br>
2021-02-13 22:03:32 -0300

    ajuste do elementos do card.

--- resources/views/components/exp_dates/card_date_historic.blade.php
+++ resources/views/components/exp_dates/card_date_historic.blade.php
@@ -1,19 +1,19 @@
-<div class="card shadow card-body min-card-width mb-4">
-    <div class="row">
-        <div class="col">
-            <h5 class="card-title">
-                <i class="far fa-file-alt"></i>
-                Historico de datas
-            </h5>
-        </div>
+<div class="card shadow min-card-width mb-4">
+    <div class="card-header">
+        <h6 class="card-title">
+            <i class="far fa-file-alt"></i>
+            Historico de datas
+        </h6>
     </div>
-    <div class="row">
-        <div class="col min-card-height-2x">
-            @if(count($historic) > 0)
-            @include('components.exp_dates.table_exp_dates', ['dates' => $dates, 'is_historic' => true])
-            @else
-            <p>Nada por aqui...</p>
-            @endif
+    <div class="card-body">
+        <div class="row">
+            <div class="col min-card-height-2x">
+                @if(count($historic) > 0)
+                @include('components.exp_dates.table_historic', ['dates' => $dates])
+                @else
+                <p>Nada por aqui...</p>
+                @endif
+            </div>
         </div>
     </div>
 </div>
